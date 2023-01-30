84bcaba19dc1af85335289e0d5b4e019c48560cd
kaacyber <ekaandriana03@gmail.com>
2023-01-30 22:19:22 +0800

    first commit

---
+++ resources/views/includes/main/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+  <div class="footer-left">
+    &copy; ROOMING 2021 - Present by <a href="https://fajarwz.netlify.app" target="_blank">Fajarwz</a> <div class="bullet"></div> Powered by Stisla 2.3.0 &copy; 2018 by <a href="https://nauval.in/" target="_blank">Muhamad Nauval Azhar</a>
+  </div>
+  <div class="footer-right">
+    1.0
+  </div>
+</footer>
